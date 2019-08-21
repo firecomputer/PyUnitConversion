7e08d6def21ce0363a280d102cd3b050e2b83366
firecomputer <jay061116@gmail.com>
2019-08-21 17:43:32 +0900

    Create unit.py
    This is unit.py's first version

---
+++ unit.py
@@ -0,0 +1,29 @@
+class length:
+  def __init__(self):  # many kinds of length unit to mm
+    self.result = 0
+    self.list = {}
+    self.list['nm'] = 0.000000000001
+    self.list['um'] = 0.000000001
+    self.list['mm'] = 0.000001
+    self.list['cm'] = 0.00001
+    self.list['inch'] = 0.000039370079
+    self.list['ja'] = 0.000303030303
+    self.list['ft'] = 0.0003048
+    self.list['yd'] = 0.0009144
+    self.list['m'] = 0.001
+    self.list['gan'] = 0.00181818182
+    self.list['jung'] = 0.109090909
+    self.list['re']0.392727273
+    self.list['km'] = 1
+    self.list['mile'] = 1.609344
+    self.list['he_re'] = 1.852
+    self.list['au'] = 149597870.7
+    self.list['light day'] = 25880431631.1
+    self.list['light year'] = 9446357545315
+    self.a = 0
+    self.b = 0
+   
+  def calc(self, unit1, unit2, count):
+    if(unit1 in self.list == True and unit2 in self.list == True):
+      self.a = self.list.get(unit1)
+      self.b = self.list.get(unit2)
